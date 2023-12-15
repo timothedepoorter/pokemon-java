c86090b522aec52755af7de6744c003909a79aa4
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2023-12-15 10:27:43 +0100

    import classe Pokemon dans attaques normal

--- src/main/java/fr/pokemon/attaque/normal/Charge.java
+++ src/main/java/fr/pokemon/attaque/normal/Charge.java
@@ -1,5 +1,7 @@
 package fr.pokemon.attaque.normal;
 
+import fr.pokemon.pokemon.Pokemon;
+
 public interface Charge {
-    public void Charge(int degatMin, int degatMax, Pokemon pokemon);
+    public void charge(int degatMin, int degatMax, Pokemon pokemon);
 }
