7bdd5b314f9262838be2f240052b14c64241939f
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2023-12-15 10:01:59 +0100

    Ajout d'attaques de bases utilisables par tous les pokemon peu import leur type

---
+++ src/main/java/fr/pokemon/attaque/normal/Charge.java
@@ -0,0 +1,5 @@
+package fr.pokemon.attaque.normal;
+
+public interface Charge {
+    public void Charge(int degatMin, int degatMax, Pokemon pokemon);
+}
---
+++ src/main/java/fr/pokemon/attaque/normal/Griffe.java
@@ -0,0 +1,5 @@
+package fr.pokemon.attaque.normal;
+
+public interface Griffe {
+    public void griffe(int degatMin, int degatMax, Pokemon pokemon);
+}
