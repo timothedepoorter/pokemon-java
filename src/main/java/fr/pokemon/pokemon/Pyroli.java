2984fee474c324494875914e2ec61205760d64bb
RayaneMeyfr <rayanemeyfr@hotmail.com>
2023-12-15 09:59:02 +0100

    feat: modif atribut type dans les class pokemon

--- src/main/java/fr/pokemon/pokemon/Pyroli.java
+++ src/main/java/fr/pokemon/pokemon/Pyroli.java
@@ -1,9 +1,10 @@
 package fr.pokemon.pokemon;
+import fr.pokemon.type.Type;
 
 import fr.pokemon.attaque.feu.LanceFlammes;
 
 public class Pyroli extends Pokemon implements LanceFlammes {
-    public Pyroli(String nom, String typePokemon, int pointVie) {
+    public Pyroli(String nom, Type typePokemon, int pointVie) {
         super(nom, typePokemon, pointVie);
     }
 
