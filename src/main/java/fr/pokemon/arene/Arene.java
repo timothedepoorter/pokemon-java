a9d00f836b1b1cf259afd6bf041a257f9ba5191d
RayaneMeyfr <rayanemeyfr@hotmail.com>
2023-12-15 10:37:45 +0100

    feat: class Arene

---
+++ src/main/java/fr/pokemon/arene/Arene.java
@@ -0,0 +1,18 @@
+package fr.pokemon.arene;
+
+public class Arene {
+    private String nom;
+
+    public Arene(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public Dresseur duel(Dresser dresser1,Dresser dresser2){
+        return null;
+    }
+
+}
