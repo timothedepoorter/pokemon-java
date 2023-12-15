7967f16fd4c9a732c22436f0887c79d946b4e10a
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2023-12-15 10:46:34 +0100

    Implemente attaque pistolet a eau Aquali

--- src/main/java/fr/pokemon/pokemon/Aquali.java
+++ src/main/java/fr/pokemon/pokemon/Aquali.java
@@ -28,8 +28,23 @@ public class Aquali extends Pokemon implements Griffe, PistoletAEau {
             pokemon.setPointVie((degatMin+degatMax) / 2);
         }
     }
+
+    /**
+     * Soustrait les points de vie du pokemon attaqué en fonction de son type
+     * (l'attaque fera plus de dégâts si le type adverse est feu, à l'inverse,
+     * elle fera moins de dégât si le type adverse est feuille).
+     * @param degatMin int - dégât minimum de l'attaque
+     * @param degatMax int - dégât maximum de l'attaque
+     * @param pokemon Pokemon - pokémon attaqué
+     */
     @Override
     public void pistoletAEau(int degatMin, int degatMax, Pokemon pokemon) {
-
+        if (pokemon.typePokemon.getNomType().equals(this.typePokemon.getForceContre())) {
+            pokemon.setPointVie(degatMax);
+        } else if (pokemon.typePokemon.getNomType().equals(this.typePokemon.getFaibleContre())) {
+            pokemon.setPointVie(degatMin);
+        } else {
+            pokemon.setPointVie((degatMin+degatMax) / 2);
+        }
     }
 }
